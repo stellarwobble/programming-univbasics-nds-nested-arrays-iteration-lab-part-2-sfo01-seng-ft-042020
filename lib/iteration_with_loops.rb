a8a2fd4471372289611ea5aaf5534c2fb7936bb2
Jennifer Paquette <jennidemuir@gmail.com>
2020-03-30 22:41:12 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -8,7 +8,7 @@ while row < src.count do
 smallest_element = []
   while element < src[row].count do
     if src[row][element] < smallest_element
-      shortest_element = src[row][element]
+      smallest_element = src[row][element]
     end
     element += 1 
   end
